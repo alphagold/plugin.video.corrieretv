1a13bbc5363afb0b7125d27822684d9ea298c2be
alphagold <30914675+alphagold@users.noreply.github.com>
2020-02-14 11:04:43 +0100

    Add files via upload

--- resources/lib/corrieretv.py
+++ resources/lib/corrieretv.py
@@ -1,11 +1,15 @@
-import urllib2
+try:
+  import urllib.request as urllib2
+except ImportError:
+    import urllib2
 import json
 import re
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
+
 
 class CorriereTV:
     __USERAGENT = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0"
-    __BaseUrl = "http://video.corriere.it"
+    __BaseUrl = "https://video.corriere.it"
     __noThumb = "http://images2.corriereobjects.it/methode_image/placeholder/320x240_A1.jpg"
 
     def __init__(self):
@@ -17,7 +21,8 @@ class CorriereTV:
     def getChannels(self):
         pageUrl = self.__BaseUrl
         data = urllib2.urlopen(pageUrl).read()
-        tree = BeautifulSoup(data, convertEntities=BeautifulSoup.HTML_ENTITIES)
+        print('cccciaoooooo')
+        tree = BeautifulSoup(data, "html.parser")
         
         list = tree.find("ul", {"id": "menu-v"}).findAll("li", recursive=False)
         channels = []
@@ -26,16 +31,15 @@ class CorriereTV:
             link = item.find("a")
             channel["title"] = link.text
             channel["url"] = link["href"]
-            
             if channel["url"] == "":
-                continue
+				continue
             if channel["url"][0:2] == "//":
-                channel["url"] = "http:" + channel["url"]
+				channel["url"] = "http:" + channel["url"]
             if channel["url"][0] == "/":
                 channel["url"] = self.__BaseUrl + channel["url"]
             if channel["url"][-1:] != "/":
                 channel["url"] = channel["url"] + "/"
-            if channel["url"] == "http://video.corriere.it/archivio/":
+            if channel["url"] == "https://video.corriere.it/archivio/":
                 continue
             
             channels.append(channel)
@@ -44,23 +48,23 @@ class CorriereTV:
 
     def getVideoByChannel(self, pageUrl):
         data = urllib2.urlopen(pageUrl).read()
-        tree = BeautifulSoup(data, convertEntities=BeautifulSoup.HTML_ENTITIES)
+        tree = BeautifulSoup(data, "html.parser")
         
         script = tree.find("script", text=re.compile("channel"))
-        match = re.search(r'channel\s*=\s*({[^;]+});', script, re.DOTALL)
+        match = re.search(r"channel\s*=\s*({[^;]+});", str(script), re.DOTALL)
         string = match.group(1)
         channel = json.loads(string)
         playlistId = channel["hierarchicalAssetProperties"]["playlistId"]
         
         pageUrl = self.__BaseUrl + "/p/" + playlistId
         data = urllib2.urlopen(pageUrl).read()
-        tree = BeautifulSoup(data, convertEntities=BeautifulSoup.HTML_ENTITIES)
+        tree = BeautifulSoup(data, "html.parser")
         
         articles = tree.findAll("article")
         videos = []
         for article in articles:
             video = {}
-            video["videoId"] = article["class"][-36:]
+            video["videoId"] = str(article["class"])[-38:-2]
             
             thumb = article.find("img")
             if thumb is not None:
